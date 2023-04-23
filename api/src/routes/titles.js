b8f5642bc85d90c0b73afbb8a09be60a136493b0
Romain Vigo Benia <romain.vigobenia@gmail.com>
2023-04-23 14:19:21 +0200

    chore(api): improve overall test code coverage

--- api/src/routes/titles.js
+++ api/src/routes/titles.js
@@ -2,9 +2,6 @@ const express = require('express')
 const router = express.Router()
 const titles = require('../controllers/titles.controller.js')
 
-// GET titles listing
-router.get('/', titles.search)
-
 // Retrieve a title
 router.get('/:tconst', titles.findOne)
 
--- api/test/services/video_creator.service.test.js
+++ api/test/services/video_creator.service.test.js
@@ -50,70 +50,91 @@ describe('#perform', () => {
       })
     })
 
-    // describe('when trailer is not found on The Movie DB', () => {
-    //   global.fetch = jest.fn(() =>
-    //     Promise.resolve({
-    //       json: () => Promise.resolve({ results: [] })
-    //     })
-    //   )
+    describe('when trailer is not found on The Movie DB', () => {
+      beforeEach(() => {
+        global.fetch = jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve({ results: [] })
+          })
+        )
+        fetch.mockClear()
+      })
 
-    //   beforeEach(() => {
-    //     fetch.mockClear()
-    //   })
+      test('returns success false', async () => {
+        const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
+        const res = await VideoCreatorServiceInstance.perform()
 
-    //   test('returns success false', async () => {
-    //     const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
-    //     const res = await VideoCreatorServiceInstance.perform()
+        expect(res.success).toBe(false)
+      })
 
-    //     expect(res.success).toBe(false)
-    //   })
+      test('returns an error', async () => {
+        const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
+        const res = await VideoCreatorServiceInstance.perform()
 
-    //   test('returns an error', async () => {
-    //     const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
-    //     const res = await VideoCreatorServiceInstance.perform()
+        expect(res.error.message).toEqual(expect.stringContaining('No video found on The Movie DB'))
+      })
+    })
 
-    //     expect(res.error.message).toEqual(expect.stringContaining('No video found on The Movie DB'))
-    //   })
-    // })
+    describe('when trailer is found on The Movie DB', () => {
+      beforeEach(() => {
+        global.fetch = jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve({
+              results: [{
+                iso_639_1: 'en',
+                iso_3166_1: 'US',
+                name: 'Fight Club - Theatrical Trailer Remastered in HD',
+                key: '6JnN1DmbqoU',
+                site: 'YouTube',
+                size: 1080,
+                type: 'Trailer',
+                official: false,
+                published_at: '2015-02-26T03:19:25.000Z',
+                id: '5e382d1b4ca676001453826d'
+              }, {
+                iso_639_1: 'en',
+                iso_3166_1: 'US',
+                name: 'Fight Club | #TBT Trailer | 20th Century FOX',
+                key: 'BdJKm16Co6M',
+                site: 'YouTube',
+                size: 1080,
+                type: 'Trailer',
+                official: true,
+                published_at: '2016-11-04T19:20:22.000Z',
+                id: '5c9294240e0a267cd516835'
+              }, {
+                iso_639_1: 'en',
+                iso_3166_1: 'US',
+                name: 'Fight Club | another trailer',
+                key: 'kJdKm16Ci8H',
+                site: 'YouTube',
+                size: 1080,
+                type: 'Trailer',
+                official: true,
+                published_at: '2014-10-02T19:20:22.000Z',
+                id: '5c926238f07d67cd516835'
+              }]
+            })
+          })
+        )
+        fetch.mockClear()
+      })
 
-    // describe('when trailer is found on The Movie DB', () => {
-    //   global.fetch = jest.fn(() =>
-    //     Promise.resolve({
-    //       json: () => Promise.resolve({
-    //         results: [{
-    //           iso_639_1: 'en',
-    //           iso_3166_1: 'US',
-    //           name: 'Fight Club - Theatrical Trailer Remastered in HD',
-    //           key: '6JnN1DmbqoU',
-    //           site: 'YouTube',
-    //           size: 1080,
-    //           type: 'Trailer',
-    //           official: false,
-    //           published_at: '2015-02-26T03:19:25.000Z',
-    //           id: '5e382d1b4ca676001453826d'
-    //         }]
-    //       })
-    //     })
-    //   )
+      test('returns success true', async () => {
+        const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
+        const res = await VideoCreatorServiceInstance.perform()
 
-    //   beforeEach(() => {
-    //     fetch.mockClear()
-    //   })
+        expect(res.success).toBe(true)
+      })
 
-    //   test('returns success true', async () => {
-    //     const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
-    //     const res = await VideoCreatorServiceInstance.perform()
+      test('returns a new video', async () => {
+        const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
+        const res = await VideoCreatorServiceInstance.perform()
 
-    //     expect(res.success).toBe(true)
-    //   })
-
-    //   test('returns a new video', async () => {
-    //     const VideoCreatorServiceInstance = new VideoCreatorService(title.tconst)
-    //     const res = await VideoCreatorServiceInstance.perform()
-
-    //     expect(res.body).toBeInstanceOf(Video)
-    //     expect(res.body.tconst).not.toBeNull()
-    //   })
-    // })
+        expect(res.body).toBeInstanceOf(Video)
+        expect(res.body.tconst).not.toBeNull()
+        expect(res.body.key).toEqual('BdJKm16Co6M')
+      })
+    })
   })
 })
