690000772cad26409ab099d2a4cb7018956ebec5
Romain Vigo Benia <romain.vigobenia@gmail.com>
2023-07-08 15:10:52 +0200

    feat: allow user to select genres

--- front/src/views/SearchSessionView.vue
+++ front/src/views/SearchSessionView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, computed } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { mainStore } from "@/stores/main";
 import { searchSessionPublics } from '@/models/search_session.model';
@@ -8,8 +8,12 @@ const router = useRouter();
 const route = useRoute();
 const store = mainStore();
 
+const mode = ref('public');
+
 const searchSessionUuid = route.params.uuid as string;
 const searchSession = computed(() => store.getSearchSessionByUuid(searchSessionUuid));
+
+const genres = ['Drama', 'Documentary', 'Comedy', 'Romance', 'Crime', 'Action', 'Thriller', 'Adventure', 'Biography', 'Family', 'History', 'Music', 'Mystery', 'Horror', 'Fantasy', 'War', 'Musical', 'Animation', 'Sport', 'Sci-Fi', 'Western']
 
 onMounted(async () => {
   await fetchData()
@@ -21,23 +25,47 @@ async function fetchData() {
 
 async function savePublic(item: string) {
   await store.updateSearchSession(searchSession.value?.uuid, { public: item } )
+  setMode('genres')
+}
+
+async function saveGenres() {
+  const selectedGenresCheckboxes = document.querySelectorAll('input[name="genres"]:checked');
+  const selectedGenres = [...selectedGenresCheckboxes].map((genre) => (genre as HTMLInputElement).value)
+  console.log(selectedGenres)
+  await store.updateSearchSession(searchSession.value?.uuid, { genres: selectedGenres } )
 }
 
 async function startProposals() {
   const newProposalUuid = await store.createProposal(searchSession.value?.uuid);
   router.push({ name: 'proposal', params: { uuid: newProposalUuid }})
 }
+
+function setMode(m: string) {
+  mode.value = m
+}
 </script>
 
 <template>
   <main>
-    <h2>Who are you watching with ?</h2>
-    <div>
-      <button v-for="item in searchSessionPublics" :key="item" @click="savePublic(item)">
-        {{ item }}
-      </button>
-      <br />
-      <button @click="startProposals">Search Proposal</button>
+    <div v-if="mode === 'public'">
+      <h2>Who are you watching with ?</h2>
+      <div>
+        <div v-for="item in searchSessionPublics" :key="item">
+          <input type="radio" name="public" :id="item" :value="item" :checked="searchSession?.public === item" @change="savePublic(item)"/>
+          <label :for="item">{{ item }}</label>
+        </div>
+        <button @click="setMode('genres')">Skip</button>
+      </div>
+    </div>
+    <div v-if="mode === 'genres'">
+      <h2>What kind of film are you in ?</h2>
+      <div>
+        <div v-for="item in genres" :key="item">
+          <input type="checkbox" name="genres" :id="item" :value="item" :checked="searchSession?.genres && searchSession?.genres.includes(item)" @change="saveGenres()">
+          <label :for="item">{{ item }}</label>
+        </div>
+        <button @click="startProposals">Start searching</button>
+      </div>
     </div>
   </main>
 </template>
