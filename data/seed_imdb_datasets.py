352d3f5eda60db2b0e86c4c4912c9306f6291f07
Romain Vigo Benia <romain.vigobenia@gmail.com>
2026-03-04 20:26:45 +0100

    feat(data): make seed script more robust when data already exists

--- data/seed_imdb_datasets.py
+++ data/seed_imdb_datasets.py
@@ -2,21 +2,14 @@ import os
 import time
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy import text
+from sqlalchemy.dialects.postgresql import insert as pg_insert
 from dotenv import load_dotenv
 
-def is_row_already_in_database(db_engine, table_name, filters):
-    conditions = ''
-    if len(filters) > 0:
-        conditions = 'WHERE'
-        processed_filters = []
-        for key, value in filters.items():
-            processed_filters.append(f" {key} = '{value}'")
-        conditions += " AND ".join(processed_filters)
+def upsert_method(table, conn, keys, data_iter):
+    insert_stmt = pg_insert(table.table).values([dict(zip(keys, row)) for row in data_iter])
+    result = conn.execute(insert_stmt.on_conflict_do_nothing())
+    return result.rowcount
 
-    query = f"SELECT EXISTS(SELECT 1 FROM {table_name} {conditions});"
-    with db_engine.connect() as connection:
-        return connection.execute(text(query)).__next__()[0]
 
 def insert_title_basics(df, db_engine):
     df.columns = ['tconst', 'title_type', 'primary_title', 'original_title', 'is_adult', 'start_year', 'end_year', 'runtime_minutes', 'genres']
@@ -29,16 +22,16 @@ def insert_title_basics(df, db_engine):
     df['runtime_minutes'] = df['runtime_minutes'].map(lambda minutes: None if (minutes == '\\N' or not(minutes.isdigit())) else minutes) # handle unknown runtime_minutes (or NaN)
     df['genres'] = df['genres'].map(lambda genres: [] if type(genres) == float else list(filter(lambda genre: genre != '\\N', genres.split(',')))) # handle wrong genres
 
-    df.to_sql('title_basics', db_engine, if_exists='append', index=False)
+    rows_inserted = df.to_sql('title_basics', db_engine, if_exists='append', index=False, method=upsert_method)
 
-    print(f'Inserted {len(df.index)} rows')
+    print(f'Inserted {rows_inserted}/{len(df.index)} rows')
 
 def insert_title_ratings(df, db_engine):
     df.columns = ['tconst', 'average_rating', 'num_votes']
 
-    df.to_sql('title_ratings', db_engine, if_exists='append', index=False)
+    rows_inserted = df.to_sql('title_ratings', db_engine, if_exists='append', index=False, method=upsert_method)
 
-    print(f'Inserted {len(df.index)} rows')
+    print(f'Inserted {rows_inserted}/{len(df.index)} rows')
 
 def insert_name_basics(df, db_engine):
     df.columns = ['nconst', 'primary_name', 'birth_year', 'death_year', 'primary_profession', 'known_for_titles']
@@ -49,9 +42,9 @@ def insert_name_basics(df, db_engine):
     df['primary_profession'] = df['primary_profession'].map(lambda primary_profession: [] if type(primary_profession) == float else primary_profession.split(','))
     df['known_for_titles'] = df['known_for_titles'].map(lambda known_for_titles: [] if type(known_for_titles) == float else known_for_titles.split(','))
 
-    df.to_sql('name_basics', db_engine, if_exists='append', index=False)
+    rows_inserted = df.to_sql('name_basics', db_engine, if_exists='append', index=False, method=upsert_method)
 
-    print(f'Inserted {len(df.index)} rows')
+    print(f'Inserted {rows_inserted}/{len(df.index)} rows')
 
 def insert_title_crew(df, db_engine):
     df.columns = ['tconst', 'directors', 'writers']
@@ -59,9 +52,9 @@ def insert_title_crew(df, db_engine):
     df['directors'] = df['directors'].map(lambda directors: list(filter(lambda director: director != '\\N', directors.split(','))))
     df['writers'] = df['writers'].map(lambda writers: list(filter(lambda writer: writer != '\\N', writers.split(','))))
 
-    df.to_sql('title_crew', db_engine, if_exists='append', index=False)
+    rows_inserted = df.to_sql('title_crew', db_engine, if_exists='append', index=False, method=upsert_method)
 
-    print(f'Inserted {len(df.index)} rows')
+    print(f'Inserted {rows_inserted}/{len(df.index)} rows')
 
 def insert_title_principals(df, db_engine):
     df.columns = ['tconst', 'ordering', 'nconst', 'category', 'job', 'characters']
@@ -69,20 +62,15 @@ def insert_title_principals(df, db_engine):
     df['job'] = df['job'].map(lambda job: None if job == '\\N' else job)
     df['characters'] = df['characters'].map(lambda characters: None if characters == '\\N' else characters)
 
-    df.to_sql('title_principals', db_engine, if_exists='append', index=False)
+    rows_inserted = df.to_sql('title_principals', db_engine, if_exists='append', index=False, method=upsert_method)
 
-    print(f'Inserted {len(df.index)} rows')
+    print(f'Inserted {rows_inserted}/{len(df.index)} rows')
 
-def import_tsv_file(file_path, db_engine, table_name, primary_key):
+def import_tsv_file(file_path, db_engine, table_name):
     reader = pd.read_csv(file_path, sep='\t', chunksize=100_000)
 
     for data_frame in reader:
-        first_row = data_frame.iloc[0]
-        filters = {attr:first_row[attr] for attr in primary_key}
-        if is_row_already_in_database(db_engine, table_name, filters):
-            print('skipping already imported batch')
-        else:
-            globals()[f"insert_{table_name}"](data_frame, db_engine)
+        globals()[f"insert_{table_name}"](data_frame, db_engine)
 
 def main():
     dbschema='imdb_datasets'
@@ -94,31 +82,31 @@ def main():
 
     print('STEP 1/5: IMPORTING title_basics')
     start_time = time.time()
-    import_tsv_file('./raw_data/title.basics.tsv', database_engine, 'title_basics', ['tconst'])
+    import_tsv_file('./raw_data/title.basics.tsv', database_engine, 'title_basics')
     end_time = time.time()
     print(f'title_basics import finished in {round(end_time-start_time)}s')
 
     print('STEP 2/5: IMPORTING title_ratings')
     start_time = time.time()
-    import_tsv_file('./raw_data/title.ratings.tsv', database_engine, 'title_ratings', ['tconst'])
+    import_tsv_file('./raw_data/title.ratings.tsv', database_engine, 'title_ratings')
     end_time = time.time()
     print(f'title_basics import finished in {round(end_time-start_time)}s')
 
     print('STEP 3/5: IMPORTING name_basics')
     start_time = time.time()
-    import_tsv_file('./raw_data/name.basics.tsv', database_engine, 'name_basics', ['nconst'])
+    import_tsv_file('./raw_data/name.basics.tsv', database_engine, 'name_basics')
     end_time = time.time()
     print(f'title_basics import finished in {round(end_time-start_time)}s')
 
     print('STEP 4/5: IMPORTING title_crew')
     start_time = time.time()
-    import_tsv_file('./raw_data/title.crew.tsv', database_engine, 'title_crew', ['tconst'])
+    import_tsv_file('./raw_data/title.crew.tsv', database_engine, 'title_crew')
     end_time = time.time()
     print(f'title_basics import finished in {round(end_time-start_time)}s')
 
     print('STEP 5/5: IMPORTING title_principals')
     start_time = time.time()
-    import_tsv_file('./raw_data/title.principals.tsv', database_engine, 'title_principals', ['tconst', 'ordering'])
+    import_tsv_file('./raw_data/title.principals.tsv', database_engine, 'title_principals')
     end_time = time.time()
     print(f'title_basics import finished in {round(end_time-start_time)}s')
 
