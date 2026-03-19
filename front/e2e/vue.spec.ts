48a04a8b7b9301ef6ff21e46cbdb0a12d3b78ec7
Romain Vigo Benia <romain.vigobenia@gmail.com>
2026-03-19 15:45:31 +0100

    feat(front): setup E2E test as Github Action

--- front/e2e/vue.spec.ts
+++ front/e2e/vue.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test'
 
 test('visits the app root url', async ({ page }) => {
   await page.goto('/')
-  await expect(page.locator('main > h1')).toHaveText("I Don't Know What To Watch")
+  await expect(page.locator('main h1')).toHaveText("I Don't Know What To Watch")
 })
